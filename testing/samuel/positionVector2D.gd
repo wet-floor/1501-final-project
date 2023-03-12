b65fffe5e621a5d8d2cfc54d9e7b09ba86fccd75
RichTheCat <destroyerofdogs@gmail.com>
2023-03-12 14:57:07 -0400

    More Level3 Changes
    - Moved .gd file to my own testing
    - Added positionVector2D

---
+++ testing/samuel/positionVector2D.gd
@@ -0,0 +1,18 @@
+extends Position2D
+
+var location = Vector2(0, 0);
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _set_location(newLocation):
+	location = newLocation
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
