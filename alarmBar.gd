9cf8c665326377fb4ef9520f6a4a6a699aa38100
RichTheCat <destroyerofdogs@gmail.com>
2023-03-12 14:44:56 -0400

    Level 3 Changes
    - Added awakeness bar
    - Added placeholder paths and markers for the awakeness threshold
    - Added placeholderColumn under testing assets

---
+++ alarmBar.gd
@@ -0,0 +1,16 @@
+extends ProgressBar
+
+var percent = 0;
+var min_percent = 0;
+var max_percent = 0;
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	min_percent = 0;
+	max_percent = 70;
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
