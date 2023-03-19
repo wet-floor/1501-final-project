6d347a7b27eed24738d3a2eb3e032735201c4a5f
h_hue <28158318+hhues@users.noreply.github.com>
2023-03-19 19:02:03 -0400

    Preliminary system
    Right-click to suck
    Left click to charge up a shot
    Release left-click
    
    Collisions are messed up...

--- testing/hendry/inventory.gd
+++ testing/hendry/inventory.gd
@@ -7,44 +7,76 @@ var currently_selected = 0
 
 # based on mouse wheel, change currently selected inventory item
 func get_input():
-	if Input.is_action_pressed("inv_left"):
+	if Input.is_action_just_released("inv_left"):
 		if currently_selected - 1 == -1:
 			currently_selected = inventory.size() - 1
 		else:
 			currently_selected -= 1
 		print(inventory[currently_selected])
+		print(get_children())
 
-	if Input.is_action_pressed("inv_right"):
+	if Input.is_action_just_released("inv_right"):
 		if currently_selected + 1 == inventory.size():
 			currently_selected = 0
 		else:
 			currently_selected += 1
 		print(inventory[currently_selected])
+		print(get_children())
 
 
-# called by the player scene, add items to the inventory
-func add_item(item : Node2D):
+# add items to the inventory
+func add_item(item : RigidBody2D):
+	# item.linear_velocity = Vector2.ZERO
+	# item.angular_velocity = 0
 	inventory.append(item)
+	if inventory[currently_selected] == null:
+		currently_selected = inventory.find(item)
+
+
+# get the currently selected item
+func get_current_item():
+	return inventory[currently_selected]
+
+
+func get_inventory():
+	return inventory
+
+
+func release_item():
+	if inventory[currently_selected] != null:
+		# var released_item : RigidBody2D 
+		# released_item = inventory[currently_selected]
+		# released_item.set_physics_process(true)
+		inventory.pop_at(currently_selected)
+		
+		currently_selected -= 1
+
+
+func release_item_at_index(index):
+	if inventory[index] != null:
+		inventory.pop_at(index)
+		
+		currently_selected -= 1
 
 
 func _physics_process(delta):
 	get_input()
 
 	# make sure all but one item is displayed
+	# 1. hide everything
 	for item in inventory:
-		var n : Node2D = item
-		item.hide()
-		item.set_physics_process(false)
-		add_child(n)
+		var n : RigidBody2D = item
+		if item != null:
+			n.hide()
+			n.collision_layer = 0
+			n.collision_mask = 0
 	
-	inventory[currently_selected].show()
-	inventory[currently_selected].set_physics_process(true)
+	# 2. show the thing that's currently selected
+	if inventory[currently_selected] != null:
+		var n : RigidBody2D = inventory[currently_selected]
+		n.show()
+		n.collision_layer = 1
+		n.collision_mask = 1
 	
-	# TODO: some inventory items don't get displayed and instead has many items within it
-	if inventory[currently_selected].body_is_in_group("stackable"):
-		pass
-
-
-func get_current_item():
-	return inventory[currently_selected]
-
+		# TODO: some inventory items don't get displayed and instead has many items within it
+		#if inventory[currently_selected].body_is_in_group("stackable"):
---
+++ testing/hendry/test_object.gd
@@ -0,0 +1,23 @@
+extends RigidBody2D
+
+var screensize
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screensize = get_viewport_rect().size
+	pass # Replace with function body.
+
+
+func _physics_process(delta):
+	position.x = clamp(position.x, 100, screensize.x - 100)
+	position.y = clamp(position.y, 100, screensize.y - 100)
+	
+	# print(position)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+	#position.x = clamp(position.x, 0, screensize.x)
+	#position.y = clamp(position.y, 0, screensize.y)
+	pass
