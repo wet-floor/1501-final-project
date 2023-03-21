2bb15064ba2004e8075fcb403905a904a05d0295
h_hue <28158318+hhues@users.noreply.github.com>
2023-03-20 23:01:12 -0400

    Better physics for inventory
    (The solution is to literally put boxes around the edge of the screen. Yay...)

--- testing/hendry/test_object.gd
+++ testing/hendry/test_object.gd
@@ -3,30 +3,11 @@ extends RigidBody2D
 var screensize
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	screensize = get_viewport_rect().size
-	pass # Replace with function body.
-
-
 func _physics_process(delta):
+	screensize = get_viewport_rect().size
 	position.x = clamp(position.x, 0, screensize.x)
 	position.y = clamp(position.y, 0, screensize.y)
 	
-	if position.x > screensize.x - 100:
-		position.x = screensize.x - 100
-	if position.x < 100:
-		position.x = 100
-	if position.y > screensize.y - 100:
-		position.y = screensize.y - 100
-	if position.y < 100:
-		position.y = 100
 	
 	# print(position)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	
-	#position.x = clamp(position.x, 0, screensize.x)
-	#position.y = clamp(position.y, 0, screensize.y)
-	pass
