62e792930cf093e0e90858df3c53548328ae35f0
sphelty <113249422+sphelty@users.noreply.github.com>
2023-03-22 05:07:53 -0400

    Object Gravity Applicable

--- testing/chloe/object.gd
+++ testing/chloe/object.gd
@@ -4,6 +4,10 @@ export (bool) var apply_gravity = false
 export (bool) var breakable = false
 export (bool) var harmful = false
 
+export (float) var gravity = 9.81
+export (float) var object_weight = 1
+export (Vector2) var velocity = Vector2(0, 1)
+
 enum {
 	FLOOR,
 	FREE,
@@ -13,25 +17,30 @@ enum {
 
 var current_state = FREE
 
-func _physics_process(delta):
+func _ready():
+	pass
+
+func _integrate_forces(state) -> void:
+	set_angular_velocity(0)
 	match current_state:
 		FLOOR:
-			floor_state(delta)
+			floor_state(state)
 		FREE:
-			free_state(delta)
+			free_state(state)
 		PLAYER:
-			player_state(delta)
+			player_state(state)
 		WALL:
-			wall_state(delta)
+			wall_state(state)
 
-func floor_state(delta):
+func floor_state(state) -> void:
 	pass
 
-func free_state(delta):
+func free_state(state) -> void:
+	if apply_gravity == true:
+		set_linear_velocity((velocity * gravity) / object_weight)
+
+func player_state(state) -> void:
 	pass
 
-func player_state(delta):
-	pass
-
-func wall_state(delta):
+func wall_state(state) -> void:
 	pass
