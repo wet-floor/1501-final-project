6010328df74f0f36bffb0fb33650fe9a08a06486
h_hue <28158318+hhues@users.noreply.github.com>
2023-03-22 23:02:47 -0400

    Final testing scene

--- testing/testing1.gd
+++ testing/testing1.gd
@@ -1,14 +1,41 @@
 extends Node
 
+var playerText = ""
+var playerName = ""
 
-onready var popupBox = $popupUI/popupBox
+var popupBox
+var textText
+var nameText
 
+onready var player = $tdPlayer
 
+# Called when the node enters the scene tree for the first time.
 func _ready():
-	popupBox.showText("[center][color=purple]Johnny", "normal text[wave amp=60 freq=6][color=red]red and wavy text[/color][/wave] [i]tiny text")	
-	popupBox.showText("[center]Alex", "[wave amp=100 freq=15]second message very wavy")
+	# Sets the nodes as variables
+	popupBox = $popupUI/popupBox
+	textText = $showTextButton/textText
+	nameText = $showTextButton/nameText
+	
+	popupBox.showText("[center][color=purple]Johnny", "I am [wave amp=60 freq=6][color=red]VERY[/color][/wave] angry right now.")	
+	popupBox.showText("[center]Alex", "[wave amp=100 freq=15]SHEEEEEEEEEEEEEEEEEEEEEEEEEEEEEESH")
 
+func _on_Button_pressed():
+	playerText = textText.text
+	playerName = nameText.text
+	popupBox.showText(playerName, playerText)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	#$inventoryUI.update_inventory(player)
+	pass
+
+func _on_Timer_timeout():
+	print("player inventory: " + str($inventoryUI.update_inventory(player)))
+	$inventoryUI.update_inventory(player)
+	var inventoryArray = player.get_inventory() as Array
+	print("gridcont children count: " + str($inventoryUI/Background/M/V/ScrollContainer/GridContainer.get_child_count()))
+	#print(inventoryArray[0])
+	#print(player.get_inventory())
+	#$inventoryUI.testing()
+	pass
+	
