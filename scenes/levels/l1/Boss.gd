e4bc7a81a28be53183972c9fe0a48a0eb5bd8c1a
h_hue <28158318+hhues@users.noreply.github.com>
2023-04-05 05:36:59 -0400

    Boss THWOMP

--- scenes/levels/l1/Boss.gd
+++ scenes/levels/l1/Boss.gd
@@ -4,7 +4,8 @@ export (int) var move_speed = 100
 export (NodePath) var follow_path
 
 # states
-enum state {chase, thwomp}
+enum States {CHASE, THWOMP, PROJECTILE}
+var state : int
 
 
 # Called when the node enters the scene tree for the first time.
@@ -12,9 +13,20 @@ func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta):
+	
+	match state:
+		States.CHASE:
+			pass
+		States.THWOMP:
+			pass
+		States.PROJECTILE:
+			pass
+
 
 func get_state():
 	return state
+
+
+func set_state(newState : int):
+	state = newState
--- scenes/levels/l1/l1-boss.gd
+++ scenes/levels/l1/l1-boss.gd
@@ -3,15 +3,40 @@ extends Node
 # nodes
 onready var boss = get_node("Boss")
 onready var player = get_node("Player")
+onready var boss_follow_path = get_node("BossFollowPath")
+onready var boss_follow_location = get_node("BossFollowPath/BossFollowLocation")
+onready var item_drop_path = get_node("ItemDropPath")
+onready var item_spawn_location = get_node("ItemDropPath/ItemSpawnLocation")
+onready var boss_animation_player = get_node("BossAnimationPlayer")
 
 # fields
 var player_position : Vector2
+var boss_state : int
+var regret_bar = 50
 
 func _ready():
 	Physics2DServer.area_set_param(get_viewport().get_world_2d().space, Physics2DServer.AREA_PARAM_GRAVITY_VECTOR, Vector2(0, 1))
-	pass # Replace with function body.
-	
+
 
 func _physics_process(delta):
 	player_position = player.global_position
-	pass
+	
+	boss_state = boss.get_state()
+	
+	match boss_state:
+		boss.States.CHASE:
+			boss_follow_location.offset = boss_follow_path.curve.get_closest_offset(player.global_position)
+			boss.global_position = lerp(boss.global_position, boss_follow_location.global_position, boss.move_speed * delta)
+			
+			# does the washing machine want to thwomp?
+		boss.States.THWOMP:
+			pass
+		boss.States.PROJECTILE:
+			pass
+
+
+func _on_Damage_body_entered(body):
+	if body == player:
+		print("washing machine hit player")
+		boss_animation_player.play("Thwomp")
+		
