33d1392fce539a28577e2ac880300f1aeb62ba20
CoffeeChr1s <christopherchallis7@gmail.com>
2023-04-05 15:22:37 -0400

    sv player
    yippie!

--- testing/chris/svPlayer.gd
+++ testing/chris/svPlayer.gd
@@ -1,18 +1,19 @@
 extends KinematicBody2D
 
-export var speed = 500
-export var friction = 0.2
-export var acceleration = 0.4
-export var gravity = 1000
-export var jumpHeight = 100
-export var jumpSpeed = 0
-export var numJumps = 0
+export var speed = 1000
+export var friction = 0.25
+export var accel = 0.3
+export var gravity = 5000
+export var jumpSpeed = -1350
+export var inertia = 100
 export var direction = "left"
 
-var input_dir = Vector2.ZERO
+var motion = Vector2.ZERO
 
 onready var hand = get_node("Hand")
 onready var inventory = get_node("Hand/Inventory")
+onready var arm = get_node("Hand/Range/CollisionPolygon2D")
+onready var charSprite = $Char
 
 var direct_range_objects = []
 var charge_power = 0
@@ -29,36 +30,71 @@ var released_object : RigidBody2D
 
 var screensize
 
+var current_scale
+var mouse
 
 # first run
 func _ready():
 	screensize = get_viewport_rect().size
+	current_scale = $Char.get_scale()
+	
 
-## PLAYER MOVEMENT 
+## player shmoovement 
 func get_input():
-	input_dir = Vector2.ZERO
+	var input_dir = 0
 
 	if Input.is_action_pressed("player_right"):
-		input_dir.x += 1
+		input_dir += 1
 	
 	if Input.is_action_pressed("player_left"):
-		input_dir.x -= 1
+		input_dir -= 1
+		
+	if input_dir != 0:
+		motion.x = lerp(motion.x, input_dir * speed, accel)
+	else:
+		motion.x = lerp(motion.x, 0, friction)
 	
 	if Input.is_action_pressed("player_jump"):
-		if numJumps<1:
-			jumpSpeed = -sqrt(jumpHeight * 2 * gravity)
-			numJumps = 1
+		if is_on_floor():
+			motion.y = jumpSpeed
+			
 	
-	input_dir = input_dir.normalized()
+	#mouse lookie wookie
+	mouse = get_global_mouse_position()
+	hand.look_at(mouse)
+	arm.rotation = hand.rotation
+	
+	#character flippy wippy
+	if mouse.x < position.x:
+		charSprite.scale.x = -abs(current_scale.x)
+		direction = "left"
+	else:
+		charSprite.scale.x = abs(current_scale.x)
+		direction = "right"
 
-# PLAYER MOVE AND LOOK
-func control():
-	# apply gravity
-	var gravity = Vector2(0, 1000)
-	input_dir = move_and_slide_with_snap(input_dir * speed, Vector2(0, -1), gravity, false, 4, 0.785398, true)
+func _physics_process(delta):
+	get_input()
+	
+	#gravity constant 
+	#god i fucking forgot a plus sign here and it took me an hour to figure out
+	motion.y = motion.y + gravity * delta
+	motion = move_and_slide(motion, Vector2.UP, false, 15, PI/3, false)
+	
+	#i think this is the for loop thing you mentioned in the meeting, tried to implement it here so it slows
+	for index in get_slide_count():
+		var collide = get_slide_collision(index)
+		if collide.collider.is_in_group("body"):
+			collide.collider.apply_central_impulse(-collide.normal * inertia)
+	
+	# sucking and shooting
+	get_action(delta)
+	
+	screensize = get_viewport_rect().size
+	position.x = clamp(position.x, 0, screensize.x)
+	position.y = clamp(position.y, 0, screensize.y)
+	
+	object_follow(delta)
 
-## PLAYER ACTIONS
-# get player input for sucking and shooting
 func get_action(delta):
 	if Input.is_action_pressed("player_suck"):
 		suck()
@@ -70,24 +106,7 @@ func get_action(delta):
 		
 	if Input.is_action_just_released("player_shoot"):
 		shoot()
-	
-	var mouse = get_global_mouse_position()
-	
-	# don't get the character confused with a close mouse distance
-	if mouse.distance_to(self.position) > 50:
-		if get_global_mouse_position().x >= (screensize.x/2):
-			face_right()
-		if get_global_mouse_position().x < (screensize.x/2):
-			face_left()
-	
-	# point the hand towards the mouse
-	if mouse.distance_to(self.position) > 100:
-		var hand_to_mouse_distance = hand.global_position.distance_to(mouse)
-		var hand_to_body_distance = hand.global_position.distance_to(self.global_position)
-		var hand_rotation = asin(hand_to_body_distance / hand_to_mouse_distance)
-		hand.rotation = -hand_rotation
-	pass
-
+		
 
 func suck():
 	if inventory.get_current_item() == null:
@@ -96,6 +115,9 @@ func suck():
 		for body in direct_range_objects:
 			var suck_impulse = (inventory.global_position - body.global_position).normalized() * suck_power
 			body.apply_central_impulse(suck_impulse)
+			
+			if body.is_in_group("container"):
+				body.eject(global_position)
 	else:
 		sucking = false
 
@@ -103,7 +125,7 @@ func suck():
 func charge_shot(delta):
 	if inventory.get_current_item() != null:
 		charge_power += charge_rate * delta
-		charge_power = clamp(charge_power, 0, 2)
+		charge_power = clamp(charge_power, 0, 1)
 		
 	pass
 
@@ -118,7 +140,7 @@ func shoot():
 			charge_power = default_charge_power
 	
 	
-		var released_object : RigidBody2D = inventory.get_current_item()
+		released_object = inventory.get_current_item()
 		
 		print(charge_power)
 		var shoot_impulse = (get_global_mouse_position() - inventory.global_position).normalized()
@@ -131,78 +153,35 @@ func shoot():
 		print(inventory.get_inventory())
 
 
+func object_follow(delta):
+	for item in inventory.get_inventory():
+		if item != null:
+			var body : RigidBody2D = item
+			body.global_position = body.global_position.linear_interpolate(inventory.global_position, delta * 80)
+			body.global_rotation = inventory.global_rotation
+
 func get_inventory():
 	return inventory.get_inventory()
 
 
 func get_currently_selected_index():
 	return inventory.get_currently_selected_index()
-	
-func object_follow(delta):
-	for item in inventory.get_inventory():
-		if item != null:
-			var body : RigidBody2D = item
-			body.global_position = body.global_position.linear_interpolate(inventory.global_position, delta * 40)
-			body.global_rotation = inventory.global_rotation
 
-func face_left():
-	if direction == "right":
-		scale.x = -1
-		direction = "left"
-func face_right():
-	if direction == "left":
-		scale.x = -1
-		direction = "right"
-
-
-## PROCESSING
-func _physics_process(delta):
-	
-	# movement and look
-	get_input()
-	# Apply gravity
-	jumpSpeed += gravity * delta
-	if jumpSpeed > 750:
-		jumpSpeed = 0
-	if jumpSpeed == 0:
-		numJumps = 0
-	
-	# Move character and detect collisions
-	var velocity = Vector2(0, jumpSpeed)
-	var collision = move_and_collide(velocity * delta)
-
-	# If there was a collision, stop the jump
-	if collision:
-		jumpSpeed = 0
-		
-	control()
-	
-	# sucking and shooting
-	get_action(delta)
-	
-	screensize = get_viewport_rect().size
-	position.x = clamp(position.x, 0, screensize.x)
-	position.y = clamp(position.y, 0, screensize.y)
-	
-	object_follow(delta)
-	
-
-
-func _process(delta):
-	pass
 
 ## SIGNALS
 func _on_Direct_Range_body_entered(body):
-	if body.is_in_group("object"):
+	if body.is_in_group("body"):
 		direct_range_objects.append(body)
 
 
 func _on_Direct_Range_body_exited(body):
-	if body.is_in_group("object"):
+	if body.is_in_group("body"):
 		direct_range_objects.erase(body)
 
 
 func _on_Inventory_body_entered(body):
 	if sucking and inventory.get_current_item() == null:
-		inventory.add_item(body)
-		print(inventory.get_inventory())
+		if body.is_in_group("object"):
+			inventory.add_item(body)
+			print(inventory.get_inventory())
+	
